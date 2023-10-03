1d939f94404786ce1a2db8738b6fdd5233c3ae98
littleblackmann <b0986816338@gmail.com>
2023-10-03 17:33:05 +0800

    「商品功能 - 實作查詢商品列表(查詢條件)」

--- springboot-mall/src/main/java/com/ltiileblack/springbootmall/dao/ProductDao.java
+++ springboot-mall/src/main/java/com/ltiileblack/springbootmall/dao/ProductDao.java
@@ -1,5 +1,6 @@
 package com.ltiileblack.springbootmall.dao;
 
+import com.ltiileblack.springbootmall.constant.ProductCategory;
 import com.ltiileblack.springbootmall.dto.ProductRequest;
 import com.ltiileblack.springbootmall.model.Product;
 
@@ -7,7 +8,7 @@ import java.util.List;
 
 public interface ProductDao {
 
-    List<Product> getProducts();
+    List<Product> getProducts(ProductCategory category, String search);
 
     Product getProductById(Integer productId);
 
--- springboot-mall/src/main/java/com/ltiileblack/springbootmall/service/impl/ProductServiceImpl.java
+++ springboot-mall/src/main/java/com/ltiileblack/springbootmall/service/impl/ProductServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ltiileblack.springbootmall.service.impl;
 
+import com.ltiileblack.springbootmall.constant.ProductCategory;
 import com.ltiileblack.springbootmall.dao.ProductDao;
 import com.ltiileblack.springbootmall.dto.ProductRequest;
 import com.ltiileblack.springbootmall.model.Product;
@@ -16,8 +17,8 @@ public class ProductServiceImpl implements ProductService {
     private ProductDao productDao;
 
     @Override
-    public List<Product> getProducts() {
-        return productDao.getProducts();
+    public List<Product> getProducts(ProductCategory category, String search) {
+        return productDao.getProducts(category,search);
     }
 
     @Override
